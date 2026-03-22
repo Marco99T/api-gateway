e14edd83b27adf7465f0e89fb72676b874cb7a2f
Marco Torres <marco.iturbide99@hotmail.es>
2026-03-22 12:01:15 -0600

    se corrige los metodos a soportar para el metodo de captura de error por medio de l circuit breaker

--- src/main/java/com/marco/torres/api_gateway/controller/FallbackController.java
+++ src/main/java/com/marco/torres/api_gateway/controller/FallbackController.java
@@ -2,13 +2,17 @@ package com.marco.torres.api_gateway.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FallbackController {
 
-    @GetMapping("/fallback/auth")
+    @RequestMapping(value = "/fallback/auth", method = {
+            RequestMethod.GET,
+            RequestMethod.POST,
+    })
     public ResponseEntity<String> authFallback() {
         return ResponseEntity
                 .status(HttpStatus.SERVICE_UNAVAILABLE)
